e07f411092d2016b0a7b13a6bbafd177171f20a8
steve garber <steve.l.garber@gmail.com>
2018-08-14 18:18:56 +0300

    carousel hover and font test run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <Carousel />
-      </div>
+        <h1>do you like this font fucktard? </h1>
+        <div class = "blurb">
+        <p> we design working pages for working folks who make products for the people. every step in our process ensures that the end user enjoys the experience of using your site. we do this by paying attention to the details and adhering to our primary principle that form must follow function. </p>
+        </div>
+    </div>
+
     );
   }
 }
