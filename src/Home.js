89dad821d0769a6da207328416351039817339a6
nachumfreedman <nochum09@gmail.com>
2018-08-27 18:05:06 +0300

    took the carousel off all the pages except home and gave the header some margin

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import Carousel from './Carousel';
 
 class Home extends Component{
   render(){
     return(
+      <div>
+        <Carousel className='home-carousel'/>
+        <h1>CapsLok</h1>
 
         <div className = "blurb">
           <p className="company-blurb"> we design working pages for working
             folks who make products for the people. every step in our process
-             ensures that the end user enjoys the experience of using your site.
-              we do this by paying attention to the details and adhering to our
-               primary principle that form must follow function. </p>
+            ensures that the end user enjoys the experience of using your site.
+            we do this by paying attention to the details and adhering to our
+            primary principle that form must follow function. </p>
         </div>
-
+      </div>
     );
   }
 }
