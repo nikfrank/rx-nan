922ee4da71db1ac07364093f33992bb5f1607397
nachumfreedman <nochum09@gmail.com>
2018-08-27 21:47:59 +0300

    bug fixes

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Carousel from './Carousel';
 
 class Home extends Component{
   render(){
--- src/Work.js
+++ src/Work.js
@@ -1,32 +1,6 @@
 import React, {Component} from 'react';
-import Modal from 'react-modal';
-import typewriter from './typewriter.jpg';
-
-const modalStyle = {
-  display: 'flex',
-  maxWidth: '30%',
-  maxHeight: '15%',
-  top: '30%',
-  left: '35%',
-  borderRadius: '5px',
-  maxWidth: '200px',
-  height: '250px',
-  ocacity: 1,
-}
-
-const website_design = 1;
 
 class Work extends Component {
-
-  state = {
-    readMore: false,
-  };
-  
-   setReadMore = (website_design) => {  
-    this.setState((state) => ({
-      readMore: website_design || false,    
-    }) )
-  };
 
   
   render() {
@@ -34,19 +8,6 @@ class Work extends Component {
     return(
       <div className='page'>
 
-        <img src={typewriter} onClick={()=>this.setReadMore(true)}/>
-        
-        <Modal
-          style={this.modalStyle}
-          shouldCloseOnOverlayClick={true}
-          onRequestClose={()=>this.setReadMore()}
-          isOpen={!!this.state.readMore}>
-          <div>
-            <button className='btn btn-default'
-                    onClick={()=>this.setReadMore()}>Close</button>
-          </div>
-        </Modal>
-        
       </div>
 
     );
