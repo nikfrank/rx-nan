5b23440882b9909aa5e76ff9af3c9187f93fd30a
nachumfreedman <nochum09@gmail.com>
2018-08-28 13:21:33 +0300

    made a tile for the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import './Home.css';
+import Blood from './imgs/blood.svg';
+import Group from './imgs/group.svg';
+import Perscription from './imgs/prescription.svg';
+import Syringe from './imgs/syringe (1).svg';
 
 class Home extends Component{
   render(){
     return(
+      
       <div className='page'>
+        <div className='home-tiles'>
+          <div className='home-jacob home-tile'>
+            <h1 style={{color:'black'}}>Help for coverage</h1>
+            <div className='home-img-div'>
+
+            </div>
+          </div>
+
+          <div className='home-page-border-1' />
+          
+          <div className='home-moran home-tile'>
+            <img src={Perscription}/>
+            <img src={Blood}/>
+            <img src={Syringe}/>
+            <img src={Group}/>
+          </div>
+
+          <div className='home-page-border-2'/>
+          
+          <div className='home-ferkin home-tile'>
+            tik tik tik tik
+          </div>
+          
+          <div className='home-page-border-1'/>
+          
+          <div className='home-moran home-tile'>
+            <h1 className='home-h1-covered'>Covered</h1>
+            <h1 className='home-h1-covered'>Covered</h1>
+            <h1 className='home-h1-not-covered'>Not Covered</h1>
+            <h1 className='home-h1-2'>2 per a day</h1>
+          </div>
+          
+
+        </div>
+
         
         <div className = "blurb" style={{backgroundColor:'black'}}>
           <div style={{color:'#C3C3E6'}}>#C3C3E6</div>
@@ -13,6 +54,7 @@ class Home extends Component{
           <div style={{color:'#F3F7F0'}}>#F3F7F0</div>
         </div>
       </div>
+
     );
   }
 }
