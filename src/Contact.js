0f1520c285584c8765e40c95b0a02e0efa4649d9
steve garber <steve.l.garber@gmail.com>
2018-08-28 11:14:48 +0300

    updated form, in process of reworking css rules for form

--- src/Contact.js
+++ src/Contact.js
@@ -27,61 +27,32 @@ class Contact extends Component {
     return(
       <div className='page'>
         <div className="form-container">
-          <div className="form-style-2">
-            <div className="form-style-2-heading">One huge step towards building your brand...</div>
-            <form action="" method="post">
-              <label htmlFor="field1">
-                <span>Name
-                  <span className="required">*</span>
-                </span>
-                <input type="text" className="input-field wider" name="field1" onChange={this.setUserName} value={userName} />
-              </label>
 
-              <label htmlFor="field2">
-                <span>Email
-                  <span className="required">*</span>
-                </span>
-                <input type="text" className="input-field wider" name="field2" onChange={this.setEmail} value={email} />
-              </label>
+          <h1 className="form-header">Send us your message here</h1>
+          <br />
+          <br />
+
+            <form id="form" class="topBefore">
+              <input id="name" type="text" placeholder="NAME" onChange={this.setUserName} value={userName}/>
+
+              <input id="email" type="text" placeholder="E-MAIL" onChange={this.setEmail} value={email}/>
 
               {
-                !this.state.isEmailValid ? (
-                  <div className='emailInvalid'>
-                    <div className='emailRetry'>
-                      Please enter your email address
+                  !this.state.isEmailValid ? (
+                    <div className='emailInvalid'>
+                      <div className='emailRetry'>
+                        Please enter your email address
+                      </div>
                     </div>
-                  </div>
-                ) :null
-              }
+                  ) :null
+                }
 
-              <label>
-                <span>Telephone</span>
-                <input type="text" className="tel-number-field" name="tel_no_1" onChange={this.setTelNo1} value={telNo1} maxLength="3" />-
-                <input type="text" className="tel-number-field" name="tel_no_2" onChange={this.setTelNo2} value={telNo2} maxLength="3"  />-
-                <input type="text" className="tel-number-field" name="tel_no_3" onChange={this.setTelNo3} value={telNo3} maxLength="4"  />
-              </label>
+              <textarea id="message" type="text" placeholder="MESSAGE" onChange={this.setMessage} value={message}/>
+              <input id="submit" type="submit" value="GO!" />
 
-              <label htmlFor="field4">
-                <span>Regarding</span>
-                <select name="field4" className="select-field">
-                  <option value="General Question">General</option>
-                  <option value="Quote">Quote</option>
-                  <option value="Careers">Careers</option>
-                </select>
-              </label>
+              </form>
 
-            <label htmlFor="field5">
-              <span>Message
-                <span className="required">*</span>
-              </span>
-              <textarea name="field5" className="textarea-field wider" onChange={this.setMessage} value={message} />
-            </label>
-
-            <label><span>&nbsp;</span><input type="submit" value="Submit" /></label>
-          </form>
-         </div>
         </div>
-
       </div>
     );
   }
