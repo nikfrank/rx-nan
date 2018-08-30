056d3b37df97710303c5c083fb29e2b616fc63cf
steve garber <steve.l.garber@gmail.com>
2018-08-30 19:52:43 +0300

    added clock svg, fixed css naming conventions  filled in content

--- src/Issues.js
+++ src/Issues.js
@@ -1,47 +1,50 @@
 import React, { Component } from "react";
 import "./Issues.css";
-
+import Sad from "./imgs/sad.svg";
 class Issues extends Component {
   render() {
     return (
       <div className="page">
         <div className="about-tiles">
-          <div className="about-jacob about-tile">
+          <div className="about-issue1 about-tile">
             <div className="about-img-div" />
             <ul>
               <li>
-                <h2>Jacob Mirrage</h2>
-              </li>
+                <h2>Patients need insulin tips</h2>
+              </li>these tips get used 1-5 times daily 5 tips X 365 days a year
+              =
               <li>
-                <h3>Senior Head of Accounts</h3>
+                <h3>1825 tips a year!</h3>
               </li>
             </ul>
           </div>
 
           <div className="about-page-border-1" />
 
-          <div className="about-moran about-tile">
+          <div className="about-issue2 about-tile">
             <ul>
               <li>
-                <h2>Moran Errad</h2>
+                <h2> Why do we need coverage?</h2>
               </li>
-              <li>
-                <h3>VP Human Resources</h3>
-              </li>
+              <li> not covering tips leads to reuse and non-use</li>
             </ul>
-            <div className="about-img-div" />
+            <div className="sadFace">
+              <img src={Sad} />
+            </div>
           </div>
 
           <div className="about-page-border-2" />
 
-          <div className="about-ferkin about-tile">
+          <div className="about-issue3 about-tile">
             <div className="about-img-div" />
             <ul>
               <li>
-                <h2>Fadi Ishmael</h2>
+                <h2> What's the rest of Canada say?</h2>
               </li>
               <li>
-                <h3>CTO</h3>
+                every other province funds these, only Ontario requires
+                Exceptional Access Paperwork
+                <h3 />
               </li>
             </ul>
           </div>
