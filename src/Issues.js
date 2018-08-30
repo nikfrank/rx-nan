ffaa621285d5459dc94b5c63dcb645b217f59cb1
steve garber <steve.l.garber@gmail.com>
2018-08-30 20:12:46 +0300

    in the middle of fixing content

--- src/Issues.js
+++ src/Issues.js
@@ -7,16 +7,14 @@ class Issues extends Component {
       <div className="page">
         <div className="about-tiles">
           <div className="about-issue1 about-tile">
-            <div className="about-img-div" />
-            <ul>
-              <li>
-                <h2>Patients need insulin tips</h2>
-              </li>these tips get used 1-5 times daily 5 tips X 365 days a year
-              =
-              <li>
-                <h3>1825 tips a year!</h3>
-              </li>
-            </ul>
+            <h2>Patients need insulin tips</h2>
+            <div>
+              <p>
+                Insulin pen tips are required for subcutaneous injection of
+                insulin
+              </p>
+              <p>The recommended needle sizes are 4mm, 5mm or 6mm</p>
+            </div>
           </div>
 
           <div className="about-page-border-1" />
@@ -26,7 +24,7 @@ class Issues extends Component {
               <li>
                 <h2> Why do we need coverage?</h2>
               </li>
-              <li> not covering tips leads to reuse and non-use</li>
+              <li> Not covering tips leads to reuse and non-use</li>
             </ul>
             <div className="sadFace">
               <img src={Sad} />
@@ -39,13 +37,16 @@ class Issues extends Component {
             <div className="about-img-div" />
             <ul>
               <li>
-                <h2> What's the rest of Canada say?</h2>
+                <h2>
+                  {" "}
+                  Why is using a clean needle for each injection important?
+                </h2>
               </li>
-              <li>
-                every other province funds these, only Ontario requires
-                Exceptional Access Paperwork
-                <h3 />
-              </li>
+              <li>To avoid clogging of the needle</li>
+              <li> To avoid bending or damage to the needle</li>
+              <li>For injection comfort</li>
+              <li>To maintain healthy injection sites</li>
+              <li>To potentially improve insuline absorption</li>
             </ul>
           </div>
         </div>
