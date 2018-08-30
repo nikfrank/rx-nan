11429dfa11a0f3e7be9209bb3fcbeb76de108579
steve garber <steve.l.garber@gmail.com>
2018-08-30 17:14:35 +0300

    fixed memory leak, finished merge

--- src/Textarea.js
+++ src/Textarea.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Textarea extends Component {
+  state = {
+    message: ""
+  };
 
-  state ={
-    message: '',
-  }
+  setValue = ({ target: { value } }) => this.setState({ message: value });
 
-  setValue = ({ target: { value } })=> this.setState({ message: value })
-  
-  
   render() {
-    console.log(this.state)  
     return (
       <div className="textarea-wrapper">
-        <textarea onChange={this.setValue} value={this.state.message} placeholder='MESSAGE' maxlength='200'style={{resize:'none'}}/>
-        {
-          this.state.message.length < 200 ?
-          <span className="textarea-counter">{this.state.message.length}/200</span> :
-          <span
-            style={{color: '#a93f55'}} className="textarea-counter">{this.state.message.length}/200</span> } 
+        <textarea
+          onChange={this.setValue}
+          value={this.state.message}
+          placeholder="MESSAGE"
+          maxLength="200"
+          style={{ resize: "none" }}
+        />
+        {this.state.message.length < 200 ? (
+          <span className="textarea-counter">
+            {this.state.message.length}/200
+          </span>
+        ) : (
+          <span style={{ color: "#a93f55" }} className="textarea-counter">
+            {this.state.message.length}/200
+          </span>
+        )}
       </div>
     );
   }
