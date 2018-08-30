40252e12652b958b9299a6a97c9372b4cbed22e0
nachumfreedman <nochum09@gmail.com>
2018-08-30 16:42:32 +0300

    changed a bunch of css names

--- src/Home.js
+++ src/Home.js
@@ -6,24 +6,22 @@ import Perscription from './imgs/prescription.svg';
 import Syringe from './imgs/syringe (1).svg';
 import Correct from './imgs/correct.svg';
 import X from './imgs/delete-button.svg';
-import Logo from './imgs/capsloklogo.svg';
 
 class Home extends Component{
   render(){
     return(
 
-      <div className='page'>
+      <div>
+        
         <div className='home-tiles'>
-          <div className='home-jacob home-tile'>
-            <h1 style={{color:'black'}}>Help for coverage</h1>
-            <div className='home-img-div'>
-
-            </div>
+          
+          <div className='home-tile-first home-tile'>
+            <h1 style={{color:'black'}}>Help for coverage</h1>          
           </div>
 
           <div className='home-page-border-1' />
 
-          <div className='home-moran home-tile'>
+          <div className='home-tile-second home-tile'>
             <img src={Perscription}/>
             <img src={Blood}/>
             <img src={Syringe}/>
@@ -32,7 +30,7 @@ class Home extends Component{
 
           <div className='home-page-border-2'/>
 
-          <div className='home-ferkin home-tile'>
+          <div className='home-tile-first home-logo-row home-tile'>
             <img src={Correct}/>
             <img src={Correct}/>
             <img src={X}/>
@@ -41,19 +39,14 @@ class Home extends Component{
 
           <div className='home-page-border-1'/>
 
-          <div className='home-moran home-tile home-tile-text'>
+          <div className='home-tile-second home-tile home-tile-text'>
             <h2 className='home-h1-covered'><br/>Covered</h2>
             <h2 className='home-h1-covered'><br/>Covered</h2>
             <h2 className='home-h1-not-covered'>Not<br/>Covered</h2>
             <h2 className='home-h1-2'>2<br/>per day</h2>
           </div>
 
-
-
-
         </div>
-
-
 
         <div className = "blurb" >
           <div style={{color:'#C3C3E6'}}>#C3C3E6</div>
@@ -62,12 +55,7 @@ class Home extends Component{
           <div style={{color:'#dddddd'}}>#dddddd</div>
           <div style={{color:'#F3F7F0'}}>#F3F7F0</div>
         </div>
-
-        <div className="foot">
-        <h2 className="credits">Nanci Frank</h2>
-        <img src ={Logo} className="logo"/>
-        </div>
-
+        
       </div>
 
       );
